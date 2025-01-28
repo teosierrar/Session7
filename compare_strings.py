1e72408dbe52f850a3bd2183ae0a5ab6e04d49da
teosierrar <teosierram@gmail.com>
2025-01-28 17:47:50 +0100

    Added compare_strings

---
+++ compare_strings.py
@@ -0,0 +1,17 @@
+s1="banana"
+s2="banana"
+
+print(s1==s2)
+s2="Banana"
+print(s1==s2)
+print(s1>s2) b>B
+s1="banana"
+s2="banany"
+print(s1>s2)
+
+#in operator can be used for strings!
+s1="I went to see Jane"
+s2="went"
+print(s2 in s1)
+print("ana" in "banana")
+print("ANA" in "banana")
