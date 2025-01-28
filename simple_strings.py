5bcb9dc160dfed0a2a438bdc94dd425d477409a4
teosierrar <teosierram@gmail.com>
2025-01-28 16:46:51 +0100

    First

---
+++ simple_strings.py
@@ -0,0 +1,12 @@
+s1="John"
+s2='Mary'
+
+print(s1)
+print(s2)
+
+s1="And then he asked:'How are you?'"
+print(s1)
+s1='And then he asked:"How are you?"'
+print(s1)
+s1="This is how to have \" and \' insid a string"
+print(s1)
